12b10240f691b42a5760e8b8f172ed7389031e18
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-24 18:41:00 -0500

    Update uvicorn_config.py

--- backend/uvicorn_config.py
+++ backend/uvicorn_config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from uvicorn.workers import UvicornWorker
 
 
@@ -13,19 +12,3 @@ class CustomWorker(UvicornWorker):
         "use_colors": True,
         "reload": True
     }
-=======
-from uvicorn.workers import UvicornWorker
-
-
-class CustomWorker(UvicornWorker):
-    CONFIG_KWARGS = {
-        "loop": "auto",
-        "http": "auto",
-        "ws": "auto",
-        "lifespan": "auto",
-        "log_level": "info",
-        "access_log": True,
-        "use_colors": True,
-        "reload": True
-    }
->>>>>>> 467f9179b7ec187f353f256c52c2ae9e8be701b2
