2c40b6c6da557422ae34c38816ad983600416f0b
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-24 19:12:16 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,8 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    # allow_origins=["*"],  # Allows all origins
+    allow_origins=["https://jubilant-barnacle.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
