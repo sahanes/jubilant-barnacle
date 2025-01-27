aeec77890d3180beebb8616b836155de52f750d9
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-27 10:18:50 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,38 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+//       }
+//     ]
+//   }
+// };
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
+  experimental: {
+    appDir: true
   },
-  async rewrites() {
+  async headers() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
-      }
-    ]
-  }
-};
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
