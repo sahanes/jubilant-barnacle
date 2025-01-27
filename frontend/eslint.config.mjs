5f4ce0d08f55ed7de7ac13e02f5f55a326a8e73e
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-27 12:37:12 -0500

    Update eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 // /** @type {import('eslint').Config} */
 // module.exports = {
 //   extends: ['next/core-web-vitals'],
@@ -61,3 +61,19 @@ const eslintConfig = [
 //     ecmaVersion: 2021
 //   }
 // };
+// .eslintrc.js
+export default {
+  extends: ['next/core-web-vitals'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn'
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2021
+  }
+};
