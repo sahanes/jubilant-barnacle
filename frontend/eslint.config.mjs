c4b6548eebca9db4c1cb5af4381d79dbdcb5789b
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-27 12:35:51 -0500

    Update eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,17 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-// ];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
 // /** @type {import('eslint').Config} */
 // module.exports = {
 //   extends: ['next/core-web-vitals'],
@@ -45,21 +45,19 @@
 //     ecmaVersion: 2021
 //   }
 // };
-/** @type {import('next').NextConfig} */
-const config = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
-
-export default config;
+// .eslintrc.js
+// export default {
+//   extends: ['next/core-web-vitals'],
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true
+//   },
+//   rules: {
+//     '@typescript-eslint/no-unused-vars': 'warn'
+//   },
+//   parserOptions: {
+//     sourceType: 'module',
+//     ecmaVersion: 2021
+//   }
+// };
