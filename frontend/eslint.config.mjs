1f8008db7faae5f521e06205acd18e63510fb980
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-27 14:33:13 -0500

    Update eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,19 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-// ];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+// *****************
+import { AppModule } from './app.module.js'
 // /** @type {import('eslint').Config} */
 // module.exports = {
 //   extends: ['next/core-web-vitals'],
