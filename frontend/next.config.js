842916e2a029a9ed60e85b5f236a0f5b50213335
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-27 12:39:54 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,8 +14,24 @@
 // // //   }
 // // // };
 
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+//         ]
+//       }
+//     ]
+//   }
+// };
 /** @type {import('next').NextConfig} */
-module.exports = {
+const config = {
   async headers() {
     return [
       {
@@ -30,3 +46,5 @@ module.exports = {
     ]
   }
 };
+
+export default config;
