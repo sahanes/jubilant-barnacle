1f05cc6bf1904a40c4358c5811e69d066cd4890f
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-27 17:26:31 -0500

    Update eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,3 +1,100 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+// // // *****************
+// // import { AppModule } from './app.module.js'
+// // /** @type {import('eslint').Config} */
+// // module.exports = {
+// //   extends: ['next/core-web-vitals'],
+// //   env: {
+// //     browser: true,
+// //     es2021: true,
+// //     node: true
+// //   },
+// //   rules: {
+// //     '@typescript-eslint/no-unused-vars': 'error'
+// //   },
+// //   parserOptions: {
+// //     sourceType: 'module'
+// //   }
+// // };
+// // const { configs } = require('@eslint/eslintrc');
+
+// // /** @type {import('eslint').Linter.Config} */
+// // module.exports = {
+// //   extends: ['next/core-web-vitals'],
+// //   env: {
+// //     browser: true,
+// //     es2021: true,
+// //     node: true
+// //   },
+// //   rules: {
+// //     '@typescript-eslint/no-unused-vars': 'warn'
+// //   },
+// //   parserOptions: {
+// //     sourceType: 'module',
+// //     ecmaVersion: 2021
+// //   }
+// // };
+// // .eslintrc.js
+// // export default {
+// //   extends: ['next/core-web-vitals'],
+// //   env: {
+// //     browser: true,
+// //     es2021: true,
+// //     node: true
+// //   },
+// //   rules: {
+// //     '@typescript-eslint/no-unused-vars': 'warn'
+// //   },
+// //   parserOptions: {
+// //     sourceType: 'module',
+// //     ecmaVersion: 2021
+// //   }
+// // };
+// // .eslintrc.js
+// // export default {
+// //   extends: ['next/core-web-vitals'],
+// //   env: {
+// //     browser: true,
+// //     es2021: true,
+// //     node: true
+// //   },
+// //   rules: {
+// //     '@typescript-eslint/no-unused-vars': 'warn'
+// //   },
+// //   parserOptions: {
+// //     sourceType: 'module',
+// //     ecmaVersion: 2021
+// //   }
+// // };
+// // module.exports = {
+// //   extends: ['next/core-web-vitals'],
+// //   // serverModuleFormat: 'cjs',
+// //   env: {
+// //     browser: true,
+// //     es2021: true,
+// //     node: true
+// //   },
+// //   rules: {
+// //     '@typescript-eslint/no-unused-vars': 'warn'
+// //   },
+// //   parserOptions: {
+// //     sourceType: 'module',
+// //     ecmaVersion: 2021
+// //   }
+// // };
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -12,86 +109,6 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
-// // *****************
-// import { AppModule } from './app.module.js'
-// /** @type {import('eslint').Config} */
-// module.exports = {
-//   extends: ['next/core-web-vitals'],
-//   env: {
-//     browser: true,
-//     es2021: true,
-//     node: true
-//   },
-//   rules: {
-//     '@typescript-eslint/no-unused-vars': 'error'
-//   },
-//   parserOptions: {
-//     sourceType: 'module'
-//   }
-// };
-// const { configs } = require('@eslint/eslintrc');
 
-// /** @type {import('eslint').Linter.Config} */
-// module.exports = {
-//   extends: ['next/core-web-vitals'],
-//   env: {
-//     browser: true,
-//     es2021: true,
-//     node: true
-//   },
-//   rules: {
-//     '@typescript-eslint/no-unused-vars': 'warn'
-//   },
-//   parserOptions: {
-//     sourceType: 'module',
-//     ecmaVersion: 2021
-//   }
-// };
-// .eslintrc.js
-// export default {
-//   extends: ['next/core-web-vitals'],
-//   env: {
-//     browser: true,
-//     es2021: true,
-//     node: true
-//   },
-//   rules: {
-//     '@typescript-eslint/no-unused-vars': 'warn'
-//   },
-//   parserOptions: {
-//     sourceType: 'module',
-//     ecmaVersion: 2021
-//   }
-// };
-// .eslintrc.js
-// export default {
-//   extends: ['next/core-web-vitals'],
-//   env: {
-//     browser: true,
-//     es2021: true,
-//     node: true
-//   },
-//   rules: {
-//     '@typescript-eslint/no-unused-vars': 'warn'
-//   },
-//   parserOptions: {
-//     sourceType: 'module',
-//     ecmaVersion: 2021
-//   }
-// };
-// module.exports = {
-//   extends: ['next/core-web-vitals'],
-//   // serverModuleFormat: 'cjs',
-//   env: {
-//     browser: true,
-//     es2021: true,
-//     node: true
-//   },
-//   rules: {
-//     '@typescript-eslint/no-unused-vars': 'warn'
-//   },
-//   parserOptions: {
-//     sourceType: 'module',
-//     ecmaVersion: 2021
-//   }
-// };
+export default eslintConfig;
+
