65061e870ec7bb1cfbe9daa68df360bf24b004f3
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-27 15:21:09 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,27 @@
-// // // /** @type {import('next').NextConfig} */
-// // // const nextConfig = {
-// // //   output: "export",
-// // //   images: {
-// // //     unoptimized: true,
-// // //   },
-// // //   async rewrites() {
-// // //     return [
-// // //       {
-// // //         source: '/api/:path*',
-// // //         destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
-// // //       }
-// // //     ]
-// // //   }
-// // // };
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+//       }
+//     ]
+//   }
+// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
 
 // /** @type {import('next').NextConfig} */
 // module.exports = {
@@ -30,21 +39,21 @@
 //     ]
 //   }
 // };
-/** @type {import('next').NextConfig} */
-const config = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
+// /** @type {import('next').NextConfig} */
+// const config = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+//           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+//         ]
+//       }
+//     ]
+//   }
+// };
 
-export default config;
+// export default config;
