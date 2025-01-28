088776fb73e6288d1a56432f0acb1cbdb42e6c78
sahanes <48871208+sahanes@users.noreply.github.com>
2025-01-28 13:38:25 -0500

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 // *************************************************************
 'use client';
 import { useState, ChangeEvent, FormEvent } from 'react';
+import 'global.css'
 
 export default function Home() {
   const [file, setFile] = useState<File | null>(null);
